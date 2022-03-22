ff4ed1d57a136f28e49ebd72bc627b66de2943db
Asriel Yu <luffy84217@gmail.com>
2022-03-22 12:33:43 +0800

    chore(config): Extend naming color scheme (#16)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Pink/Purple Tailwind Color Scheme
+        primary: '#f472b6',
+        'primary-dark': '#c084fc',
+        secondary: '#ffe4e6',
+        'secondary-dark': '#f3e8ff',
+        accent: '#ec4899',
+        'accent-dark': '#a855f7',
+        // Custom Color Scheme
+        danger: '#ff4444',
+        'danger-dark': '#cc0000',
+        warning: '#ffbb33',
+        'warning-dark': '#ff8800',
+        success: '#00c851',
+        'success-dark': '#007e33',
+        info: '#33b5e5',
+        'info-dark': '#0099cc',
+        // Neutral Tailwind Color Scheme
+        background: '#fafafa',
+        'background-dark': '#171717',
+        color: '#171717',
+        'color-dark': '#fafafa',
+      },
+    },
   },
   plugins: [],
 }
